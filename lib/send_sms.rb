a7838edddf5fb78210afb2fa84d9fb533cf5a5c0
Abdullah <adaoub0@gmail.com>
2022-09-01 11:29:10 +0100

    .env added

--- lib/send_sms.rb
+++ lib/send_sms.rb
@@ -2,9 +2,6 @@ require "twilio-ruby"
 require "dotenv/load"
 
 class SendSms
-  # @account_sid = "AC21f846a94d9b08e5971a59fabbf92685"
-  # @auth_token = "2313958e1c0222c200202996cb0279a1"
-
   def initialize(client)
     @client = client
   end
